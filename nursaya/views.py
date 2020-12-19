3efa26ef5ef160169b83740fb3f2ed4003023ebd
Sana1993-soo <nursayauskembay@gmail.com>
2020-12-19 23:55:21 +0600

    First commit

---
+++ nursaya/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def about(request):
+    return HttpResponse('This is about page')
+
+def home(request):
+    return HttpResponse('<h1>This is about home</h1>')
+
